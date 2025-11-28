f41b7aea104cf17c56d7305b1edc056c79c9e402
ariel <arielbruckertneant@gmail.com>
2025-11-28 10:46:24 +0100

    init commit

---
+++ src/main/java/org/example/Employe.java
@@ -0,0 +1,35 @@
+package compagnieAerienne;
+
+public class Employe extends Personne {
+
+    private int numeroEmploye;
+    private String dateEmbauche; // pour l’instant en String, simple
+
+    public Employe(int identifiant, String nom, String adresse, String contact,
+                   int numeroEmploye, String dateEmbauche) {
+        super(identifiant, nom, adresse, contact);
+        this.numeroEmploye = numeroEmploye;
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public int getNumeroEmploye() {
+        return numeroEmploye;
+    }
+
+    public void setNumeroEmploye(int numeroEmploye) {
+        this.numeroEmploye = numeroEmploye;
+    }
+
+    public String getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(String dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    // Méthode demandée : sera redéfinie dans les sous-classes
+    public String obtenirRole() {
+        return "Employé";
+    }
+}
